3f8b65425573060541ce71fd6458837de7614f39
Michae1Mao <northpark46@gmail.com>
2018-06-17 10:29:43 +0800

    add anuity cal

--- ficalculator.py
+++ ficalculator.py
@@ -1,7 +1,12 @@
-def Current_value(PA,rate,t):
+def Current_value(Anuity,rate,t):
     temp = 0
     for n in range(t):
-        temp = temp + PA/(pow(1+rate,n+1))
-        print(n)
+        temp = temp + Anuity/(pow(1+rate,n+1))
     return temp
 
+
+def CalAnuity(CV,rate,t):
+    temp = 0
+    for n in range(t):
+        temp = temp + 1/(pow(1+rate,n+1))
+    return CV/temp
